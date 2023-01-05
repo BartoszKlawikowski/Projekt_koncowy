59ce42abcca9129f6837bbf40a1fcf4942048baa
BartoszKlawikowski <lewbartosz@gmail.com>
2023-01-05 14:49:23 +0100

    Test
    test

---
+++ Projekt_końcowy/main.cpp
@@ -0,0 +1,13 @@
+/*
+Temat projektu: Gra statki kosmiczne
+Zakres indywidualny: too be annouced
+*/
+#include <iostream>
+
+int main() 
+{
+	std::cout << "Hellow World!";
+	std::cout << "Hellow World!";
+	std::cout << "Hellow World!";
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Projekt_koncowy
  
+Temat projektu: Gra statki kosmiczne
+Zakres indywidualny: to be annouced
+
+
+12
